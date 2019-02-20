698489ec66df7f0fa41d31498197050914525499
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2019-02-20 19:41:53 -0300

    Adição de arquivos

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+
+const users = ['Eduardo', 'Robson', 'Cleiton']
+
+app.get('/', (req, res) => {
+  return res.render('list', { users })
+})
+
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  return res.redirect('/')
+})
+
+app.listen(80, () => {
+  console.log('App running at port 80')
+})
